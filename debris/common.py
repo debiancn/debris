614e9d4676bff4fff1bf802adc53d242e5120fe0
Boyuan Yang <073plan@gmail.com>
2016-12-04 10:35:48 +0800

    record some sbuild usage

--- debris/common.py
+++ debris/common.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 #
 # debris.common -- common things for debris autobuild system
-pass
 
 import configparser
+import subprocess
 
 def load_config(filepath: str) -> configparser.ConfigParser:
     """
@@ -18,3 +18,61 @@ def load_config(filepath: str) -> configparser.ConfigParser:
         raise
 # TODO: exception handler
     return config
+
+def getconfig(config_key: str, returntype: type = str):
+    """
+    Obtain config information.
+
+    Priority:
+     * envvar
+     * config file
+     * builtin
+    """
+    BUILTIN_CONFIG = {
+            'DEBRIS_DB_FILE' : '/var/cache/debris/history.db',
+            'DEBRIS_SBUILD_MIRRORURI' : 'http://ftp2.cn.debian.org/debian',
+            'DEBRIS_SBUILD_EXTRAURI' : 'http://repo.debiancn.org/',
+            'DEBRIS_SBUILD_CHROOT_ARCH' : 'amd64',
+            'DEBRIS_SBUILD_CHROOT_SUITE' : 'stretch',
+            'DEBRIS_SBUILD_CHROOT_SUFFIX' : 'sbuild',
+            'DEBRIS_SBUILD_CHROOT_TARGET_DIRECTORY_BASE': '/var/cache/debris/',
+            'DEBRIS_GIT_REPO_URL' : 'https://github.com/debiancn/repo',
+            'DEBRIS_GIT_REPO_LOCAL' : '/home/hosiet/src/debiancn/repo',
+            }
+
+
+    config = BUILTIN_CONFIG
+    # TODO: load config file here
+    # TODO: load envvar here
+
+    if config_key in config.keys():
+        return returntype(config[config_key])
+    else:
+        raise Exception('ERR_CONFIG_KEY_NOT_RECOGNIZED')
+
+def run_process(arglist, timeout=None) -> subprocess.CompletedProcess:
+    """
+    Wrapper for subprocess.run()
+
+    Require python 3.5+
+    """
+    try:
+        result = subprocess.run(
+                arglist,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                timeout=timeout,
+                check=True,
+                );
+    except subprocess.TimeoutExpired as e:
+        # TODO: deal with it
+        raise
+        return e
+    except subprocess.CalledProcessError as e:
+        # TODO: deal with it
+        raise
+        return e
+    except:
+        raise
+
+    return result
--- debris/sbuild.py
+++ debris/sbuild.py
@@ -5,7 +5,10 @@
 from . import common
 import configparser
 
-class Sbuilder(object):
+from .common import run_process
+from .common import getconfig
+
+class SBuilder(object):
     class SBInstance(object):
         """
         The instance to represent the schroot instance.
@@ -13,28 +16,48 @@ class Sbuilder(object):
         Note: self.ready is a bool. True or False or None.
         TODO: use subprocess + communicate() to obtain information.
         """
-        def __init__(self, path: str, arch: str, suite: str):
-            self.path = path
-            self.arch = arch
-            self.suite = suite
+        def __init__(self, CHROOT: str = None, arch: str = None, suite: str = None):
+            if CHROOT:
+                self.arch = CHROOT.split('-')[1]
+                self.suite = CHROOT.split('-')[0]
+                self.chroot = CHROOT
+            elif not arch or not suite:
+                raise Exception('ERR_INVALID_SBInstance')
+            else:
+                self.arch = arch
+                self.suite = suite
+                self.chroot = '-'.join(
+                        (self.suite,
+                         self.arch,
+                         getconfig('DEBRIS_SBUILD_CHROOT_SUFFIX', str),
+                        ))
             self.ready = None
         
         def _update(self):
             """
             Update the instance by calling 'sbuild-update'.
+
+            NOTE: By default, we are modifying the original "source:"-prefixed
+            chroot, which means we need permission called source-root-users in
+            config file.
+            
+            Same notice applies to the following methods.
             """
+            run_process(['sbuild-update', self.chroot], 1800)
             pass
 
         def _upgrade(self):
             """
             Call 'sbuild-update --upgrade'.
             """
+            run_process(['sbuild-update', '--upgrade', self.chroot], 3600)
             pass
 
         def _full_upgrade(self):
             """
             Call 'sbuild-update --dist-upgrade'.
             """
+            run_process(['sbuild-update', '--dist-upgrade', self.chroot], 3600)
             pass
 
         def _dist_upgrade(self):
@@ -51,9 +74,17 @@ class Sbuilder(object):
             self._full_upgrade()
             pass
 
-        def buildpkg(self, keyfilepath : str, type: str = "dsc"):
+        def buildpkg(self, keyfilepath : str, buildtype: str = "dsc"):
+            # prefer using arch + suite rather than hardcoded schroot option
+            if buildtype == "dsc":
+                if self.arch and self.suite:
+                    run_process([
+                        'sbuild',
+                        '--dist={}'.format(self.suite),
+                        '--arch={}'.format(self.arch),
+                        keyfilepath.dsc,
+                        ])
             pass
-
 
 
     def __init__(self, config: configparser.ConfigParser):
