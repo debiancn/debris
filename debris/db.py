f23fe56b3b93ef27b24e656bca9997edc280e856
Boyuan Yang <073plan@gmail.com>
2016-12-07 12:28:45 +0800

    initial write sth about db

--- debris/db.py
+++ debris/db.py
@@ -30,6 +30,17 @@ class DebrisDB(object):
         self.conn = sqlite3.connect(my_dbpath)
         # TODO: Complete me
 
+    def _sanity_check(self):
+        """Run a sanity check.
+
+        If there are any missing tables, create them.
+        """
+        c = self.cursor()
+        c.execute('CREATE TABLE IF NOT EXISTS `builtpkg` (`package` TEXT, `version` TEXT);')
+        c.execute('CREATE TABLE IF NOT EXISTS `command_history` (`timestamp` INTEGER, `CMDTYPE` TEXT, `OPERATION` TEXT);')
+# TODO: recheck this
+        pass
+
     def get_builtlist(self) -> list:
         """Retrieve a list for previously built packages.
 
@@ -37,6 +48,10 @@ class DebrisDB(object):
             [{'package': 'nixnote2', 'version': '2.0~beta9-1'},
              {'package': 'qevercloud', 'version': '3.0.3+ds-1'}]
         """
-        return []
+        builtlist = []
+        c = self.cursor()
+        result = c.execute('SELECT * FROM `builtpkg`;').fetchall()
+# TODO: finish me
+        return builtlist
         pass
 # TODO FIXME
